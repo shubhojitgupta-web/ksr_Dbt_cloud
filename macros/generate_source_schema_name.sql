713eec6fa225f2aaa626ab5830d6c0929430fbb5
KSR DBT <kewof74878@bialode.com>
2025-12-29 04:47:47 +0000

    new

--- macros/generate_source_schema_name.sql
+++ macros/generate_source_schema_name.sql
@@ -1,7 +1,9 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
     {% if custom_schema_name is none %}
         {{ target.schema }}
+    {% elif custom_schema_name | lower == 'landing' %}
+        {{ custom_schema_name }}
     {% else %}
-        {{ target.name }}_{{ custom_schema_name }}
+        {{ target.schema }}_{{ custom_schema_name }}
     {% endif %}
 {% endmacro %}
